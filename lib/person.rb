adc93163933ff7e9c861455c272e374bd3bd2b9c
Duong Dang <dddangmedia@gmail.com>
2019-11-02 02:20:00 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person 
+  def name=(given_name)
+    @name = given_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(given_job)
+    @current_job = given_job
+  end
+  
+  def job
+    @current_job
+  end
+end

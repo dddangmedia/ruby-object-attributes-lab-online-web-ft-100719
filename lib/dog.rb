03ef9b0e3bdae16cfcfe86b59b16fe94e9e3826d
Duong Dang <dddangmedia@gmail.com>
2019-11-02 02:18:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,4 +2,16 @@ class Dog
   def name=(given_name)
     @name = given_name
   end
+  
+  def name 
+    @name
+  end
+  
+  def breed=(dog_breed)
+   @breed = dog_breed
+  end
+  
+  def breed
+    @breed
+  end
 end
